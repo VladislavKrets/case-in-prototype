e2b63cfbb7d90bbc5e172a9d59c5e647368d14d6
Vladislav Krets <vladislav.krets@gmail.com>
2022-05-20 18:32:45 +0300

    added osm maps

--- src/Maps/Maps.js
+++ src/Maps/Maps.js
@@ -1,56 +1,46 @@
-import React, {useEffect, useState} from "react";
-import {GoogleMap, useJsApiLoader, Marker, InfoWindow, DirectionsRenderer} from '@react-google-maps/api';
+import React, {useEffect, useState, useRef} from "react";
 import transportSVG from "../assets/record2.svg"
 import {useParams} from "react-router";
+import {
+    TileLayer,
+    MapContainer,
+    LayersControl, Marker, Popup, useMapEvents, useMap
+} from "react-leaflet";
+
+import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+
+import 'leaflet-routing-machine'
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
+
+import "./Maps.css"
+
+const maps = {
+    base: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+};
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 function Maps(props) {
     let {id} = useParams();
+    const [map, setMap] = React.useState(null)
+
     const containerStyle = {
         width: '100%',
         height: '100%',
         cursor: props.isPointsAdditionState ? 'crosshair' : undefined
     };
 
-    const [center, setCenter] = useState({
-        lat: 63.259060,
-        lng: 125.273935
-    });
-
-    const {isLoaded} = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: "AIzaSyCHazPeqHjDMS5-0IpU-fN3_P_ixxN-wqo"
-    })
-
-    const [map, setMap] = React.useState(null)
-    const [isAllLoaded, setAllLoaded] = useState(false);
-    const onLoad = React.useCallback(function callback(map) {
-        const bounds = new window.google.maps.LatLngBounds();
-        map.fitBounds(bounds);
-        setMap(map)
-    }, [])
-
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-    }, [])
-
-    const [zoom, setZoom] = useState(10);
-    useEffect(() => {
-        if (!isAllLoaded)
-            setTimeout(() => {
-                const elements = document.querySelectorAll('img[src="https://maps.gstatic.com/mapfiles/api-3/images/google_gray.svg"]');
-                elements.forEach((element) => {
-                    const parent = element.parentElement.parentElement;
-                    parent.remove();
-                })
-                setMap(map);
-                setAllLoaded(true);
-                setZoom(4);
-                setCenter({
-                    lat: 63.259060,
-                    lng: 125.273935
-                })
-            }, 800);
-    }, [map])
+    const [center, setCenter] = useState([
+        63.259060,
+        125.273935
+    ]);
 
     const [popups, setPopups] = useState(props.objects[id - 1].transport.map(item => false));
 
@@ -58,118 +48,94 @@ function Maps(props) {
         setPopups(popups.map(item => false))
     }, [props.objects[id - 1].transport])
 
-    const [directions, setDirections] = useState([])
 
-    const getDirections = (origin, destination) => {
-        const directionsService = new window.google.maps.DirectionsService();
+    function Routing(props) {
+        const map = useMap();
+        useEffect(() => {
+            if (!map) return;
 
-        if (origin !== null && destination !== null) {
-            directionsService.route(
-                {
-                    origin: origin,
-                    destination: destination,
-                    travelMode: window.google.maps.TravelMode.DRIVING
-                },
-                (result, status) => {
-                    if (status === window.google.maps.DirectionsStatus.OK) {
-                        const newDirections = [...directions]
-                        newDirections.push(result)
-                        setDirections(newDirections)
-                    } else {
-                        console.error(`error fetching directions ${result}`);
-                    }
-                }
-            );
-        } else {
-            console.log('Please mark your destination in the map first!');
-        }
-    };
+            const routingControl = L.Routing.control({
+                waypoints: props.userMapPoints.map(item => L.latLng(item.lat, item.lng)),
+                routeWhileDragging: true
+            }).addTo(map);
+
+            return () => map.removeControl(routingControl);
+        }, [map]);
+
+        return <></>;
+    }
 
     const onMapClick = (event) => {
         if (props.isPointsAdditionState) {
-            const lat = event.latLng.lat();
-            const lng = event.latLng.lng();
+            const lat = event.latlng.lat;
+            const lng = event.latlng.lng;
             const userMapPoints = [...props.userMapPoints]
             userMapPoints.push({lat: lat, lng: lng})
             props.setUserMapPoints(userMapPoints)
             props.setPointsAdditionState(false);
-            if (userMapPoints.length > 1) {
-                getDirections(userMapPoints[userMapPoints.length - 2], userMapPoints[userMapPoints.length - 1])
-            }
         }
     }
 
+
+    const CoordsLayer = () => {
+        const mapEvents = useMapEvents({
+            click(e) {
+                onMapClick(e);
+            },
+        })
+        return (
+            <></>
+        )
+
+    }
+
     return <div style={{flex: '1 1 auto'}}>
-        {
-            isLoaded ? (
-                <GoogleMap
-                    options={{draggableCursor: props.isPointsAdditionState ? 'crosshair' : ""}}
-                    draggable={!props.isPointsAdditionState}
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={zoom}
-                    onLoad={onLoad}
-                    onUnmount={onUnmount}
-                    onClick={onMapClick}
-                >
-                    {
-                        props.objects[id - 1].transport.filter(item => item.isMarker).map((item, index) => {
-                            return <Marker
-                                key={index}
-                                position={{lat: item.lat, lng: item.lng}}
-                                icon={{
-                                    url: transportSVG,
-                                    anchor: new window.google.maps.Point(5, 58),
-                                    scaledSize: new window.google.maps.Size(40, 40)
-                                }}
-                                onClick={() => {
-                                    const newPopups = [...popups];
-                                    newPopups[index] = !newPopups[index]
-                                    setPopups(newPopups)
-                                    setCenter({
-                                        lat: item.lat,
-                                        lng: item.lng
-                                    })
-                                }}
-                            >
-                                {item.isMarker && popups[index] && <InfoWindow
-                                    onCloseClick={() => {
-                                        const newPopups = [...popups];
-                                        newPopups[index] = false;
-                                        setPopups(newPopups);
-                                    }}
-                                    visible={popups[index]}>
-                                    <div>
-                                        <div style={{textAlign: 'center', fontWeight: 'bold'}}>{item.name}</div>
-                                        <div
-                                            style={{whiteSpace: "pre-line", lineHeight: '2em'}}>{item.description}</div>
-                                    </div>
-                                </InfoWindow>}
-                            </Marker>
-                        })
-                    }
-                    {
-                        props.userMapPoints.map((item, index) => {
-                            return <Marker
-                                key={index}
-                                position={{lat: item.lat, lng: item.lng}}>
-                            </Marker>
-                        })
-                    }
-                    {
-                        directions.map((item, index) => {
-                            return <DirectionsRenderer
-                                key={index}
-                                directions={item}
-                                defaultOptions={{
-                                    suppressMarkers: true
-                                }}
-                            />
-                        })
-                    }
-                </GoogleMap>
-            ) : <></>
-        }
+        <MapContainer
+            center={center}
+            zoom={5}
+            zoomControl={true}
+            style={containerStyle}
+            doubleClickZoom
+            whenCreated={map => setMap(map)}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {
+                props.objects[id - 1].transport.filter(item => item.isMarker).map((item, index) => {
+                    return <Marker
+                        key={index}
+                        position={[item.lat, item.lng]}
+                        icon={L.icon({
+                            iconUrl: transportSVG,
+                            iconAnchor: [5, 58],
+                            iconSize: [40, 40],
+                        })}
+                    >
+                        <Popup>
+                            <div style={{width: '250px'}}>
+                                <div style={{textAlign: 'center', fontWeight: 'bold'}}>{item.name}</div>
+                                <div
+                                    style={{whiteSpace: "pre-line", lineHeight: '2em'}}>{item.description}</div>
+                            </div>
+                        </Popup>
+                    </Marker>
+                })
+            }
+            {
+                props.userMapPoints.map((item, index) => {
+                    return <Marker
+                        key={index}
+                        position={{lat: item.lat, lng: item.lng}}>
+                    </Marker>
+                })
+            }
+            {
+                <Routing userMapPoints={props.userMapPoints}/>
+            }
+            <CoordsLayer/>
+        </MapContainer>
     </div>
 }
 
