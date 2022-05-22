a40f0783cf4cbd180ce6e5f8bb8fa3b738e45b8b
Vladislav Krets <vladislav.krets@gmail.com>
2022-05-22 22:23:41 +0300

    video changed

--- src/Video/Video.js
+++ src/Video/Video.js
@@ -5,7 +5,7 @@ export default function Video(props) {
     return <div className={'video-container'}>
         {!props.isCamChecked.reduce((prev, current) => prev || current, false) ? <div className={'video'}>
             Для начала работы выберите камеры
-        </div> : <iframe style={{width: '100%', height: '100%'}} src="https://www.youtube.com/embed/NK_71ggNeik?autoplay=1&mute=1"
+        </div> : <iframe style={{width: '100%', height: '100%'}} src="https://www.youtube.com/embed/dcHI_qFzI4c?autoplay=1&mute=1"
                          title="YouTube video player" frameBorder="0"
                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                          allowFullScreen></iframe>}
